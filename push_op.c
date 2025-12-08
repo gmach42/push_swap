369bbca4a85851e0ccb3b34b1f06c6c8d78fe5d0
gmach <gmach@student.42lyon.fr>
2025-12-08 18:45:18 +0100

    push_op update

--- push_op.c
+++ push_op.c
@@ -5,9 +5,36 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: gmach <gmach@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/12/08 18:17:28 by gmach             #+#    #+#             */
-/*   Updated: 2025/12/08 18:19:23 by gmach            ###   ########lyon.fr   */
+/*   Created: 2025/12/08 18:29:57 by gmach             #+#    #+#             */
+/*   Updated: 2025/12/08 18:44:49 by gmach            ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+void	push_op(t_stack **dest, t_stack **src)
+{
+	t_stack	*temp;
+
+	if (!src || !*src || !dest)
+		return ;
+	temp = *src;
+	*src = (*src)->next;
+	temp->next = *dest;
+	*dest = temp;
+}
+void	pa(t_stack **stack_a, t_stack **stack_b)
+{
+	if (!*stack_b)
+		return ;
+	push_op(stack_a, stack_b);
+	ft_printf("pa\n");
+}
+
+void	pb(t_stack **stack_a, t_stack **stack_b)
+{
+	if (!*stack_a)
+		return ;
+	push_op(stack_b, stack_a);
+	ft_printf("pb\n");
+}
